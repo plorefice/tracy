6ce4e34d1c36fe34204a3ecdd5f9b6ae18717316
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-02-06 23:56:07 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+//! A Rust implementation of The Ray Tracer Challenge book.
+
+#![deny(missing_debug_implementations)]
+#![warn(missing_docs)]
+
+use std::f32::EPSILON;
+
+/// A four-dimensional `(x,y,z,w)` tuple  that can represent a point or vector in 3D space.
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
+pub struct Tuple {
+    /// The `x` component of this tuple.
+    pub x: f32,
+    /// The `y` component of this tuple.
+    pub y: f32,
+    /// The `z` component of this tuple.
+    pub z: f32,
+    /// The `w` component of this tuple.
+    pub w: f32,
+}
+
+impl From<(f32, f32, f32, f32)> for Tuple {
+    fn from((x, y, z, w): (f32, f32, f32, f32)) -> Self {
+        Self { x, y, z, w }
+    }
+}
+
+impl Tuple {
+    /// Creates a new tuple from the coordinates of a point in space.
+    pub fn from_point(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z, w: 1.0 }
+    }
+
+    /// Creates a new tuple from the coordinates of a vector in space.
+    pub fn from_vector(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z, w: 0.0 }
+    }
+
+    /// Checks whether this tuple represents a point, ie. its `w` component is equal to 1.
+    pub fn is_point(&self) -> bool {
+        (self.w - 1.0).abs() <= EPSILON
+    }
+
+    /// Checks whether this tuple represents a vector, ie. its `w` component is equal to 0.
+    pub fn is_vector(&self) -> bool {
+        self.w == 0.0
+    }
+}
---
+++ tests/ch01.rs
@@ -0,0 +1,82 @@
+use std::{convert::Infallible, f32::EPSILON};
+
+use cucumber_rust::{async_trait, given, then, World, WorldInit};
+use trtc::Tuple;
+
+#[derive(WorldInit)]
+pub struct Runner {
+    a: Tuple,
+    p: Tuple,
+    v: Tuple,
+}
+
+#[async_trait(?Send)]
+impl World for Runner {
+    type Error = Infallible;
+
+    async fn new() -> Result<Self, Infallible> {
+        Ok(Self {
+            a: Default::default(),
+            p: Default::default(),
+            v: Default::default(),
+        })
+    }
+}
+
+#[given(regex = r"a ← tuple\(([0-9.-]+), ([0-9.-]+), ([0-9.-]+), ([0-9.-]+)\)")]
+async fn given_a_tuple(runner: &mut Runner, x: f32, y: f32, z: f32, w: f32) {
+    runner.a = (x, y, z, w).into();
+}
+
+#[given(regex = r"p ← point\(([0-9.-]+), ([0-9.-]+), ([0-9.-]+)\)")]
+async fn given_a_point(runner: &mut Runner, x: f32, y: f32, z: f32) {
+    runner.p = Tuple::from_point(x, y, z);
+}
+
+#[given(regex = r"v ← vector\(([0-9.-]+), ([0-9.-]+), ([0-9.-]+)\)")]
+async fn given_a_vector(runner: &mut Runner, x: f32, y: f32, z: f32) {
+    runner.v = Tuple::from_vector(x, y, z);
+}
+
+#[then(regex = r"a.(\w) = ([0-9.-]+)")]
+async fn tuple_field_equals_value(runner: &mut Runner, field: char, value: f32) {
+    match field {
+        'x' => assert!((runner.a.x - value).abs() < EPSILON),
+        'y' => assert!((runner.a.y - value).abs() < EPSILON),
+        'z' => assert!((runner.a.z - value).abs() < EPSILON),
+        'w' => assert!((runner.a.w - value).abs() < EPSILON),
+        _ => unreachable!("invalid tuple field"),
+    }
+}
+
+#[then(regex = r"a is (not )?a point")]
+async fn tuple_is_point(runner: &mut Runner, not_str: String) {
+    assert_eq!(runner.a.is_point(), not_str.is_empty())
+}
+
+#[then(regex = r"a is (not )?a vector")]
+async fn tuple_is_vector(runner: &mut Runner, not_str: String) {
+    assert_eq!(runner.a.is_vector(), not_str.is_empty())
+}
+
+#[then(regex = r"p = tuple\(([0-9.-]+), ([0-9.-]+), ([0-9.-]+), ([0-9.-]+)\)")]
+async fn point_is_tuple(runner: &mut Runner, x: f32, y: f32, z: f32, w: f32) {
+    assert!((runner.p.x - x).abs() < EPSILON);
+    assert!((runner.p.y - y).abs() < EPSILON);
+    assert!((runner.p.z - z).abs() < EPSILON);
+    assert!((runner.p.w - w).abs() < EPSILON);
+}
+
+#[then(regex = r"v = tuple\(([0-9.-]+), ([0-9.-]+), ([0-9.-]+), ([0-9.-]+)\)")]
+async fn vector_is_tuple(runner: &mut Runner, x: f32, y: f32, z: f32, w: f32) {
+    assert!((runner.v.x - x).abs() < EPSILON);
+    assert!((runner.v.y - y).abs() < EPSILON);
+    assert!((runner.v.z - z).abs() < EPSILON);
+    assert!((runner.v.w - w).abs() < EPSILON);
+}
+
+#[tokio::main]
+async fn main() {
+    let runner = Runner::init(&["./features"]);
+    runner.run_and_exit().await;
+}
