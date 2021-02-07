8ec72f7a19f88abcd9bb0e003387315ae174143e
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-02-07 20:10:24 +0100

    ch01: render projectile trajectory

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 
 pub mod math;
 
+use math::Coords;
 use wasm_bindgen::{prelude::*, Clamped};
 use web_sys::{CanvasRenderingContext2d, ImageData};
 
@@ -20,15 +21,34 @@ pub fn draw(ctx: &CanvasRenderingContext2d, width: u32, height: u32) -> Result<(
 }
 
 fn render_projectile(width: usize, height: usize) -> Vec<u8> {
-    let mut raw_image = Vec::with_capacity(width * height);
+    let mut raw_image = Vec::with_capacity(width * height * 4);
 
+    // Fill with black background
     for _ in 0..height {
         for _ in 0..width {
-            raw_image.push(255);
+            raw_image.push(0);
             raw_image.push(0);
             raw_image.push(0);
             raw_image.push(255);
         }
+    }
+
+    let mut pos = Coords::from_point(0., 1., 0.);
+    let mut vel = Coords::from_vector(1., 1., 0.);
+
+    let gravity = Coords::from_vector(0., -0.1, 0.);
+    let wind = Coords::from_vector(-0.03, 0., 0.);
+
+    while pos.y > 0. {
+        let cx = (pos.x * 15.) as usize;
+        let cy = height - (pos.y * 15.) as usize;
+
+        raw_image[cy * 4 * width + cx * 4] = 255;
+        raw_image[cy * 4 * width + cx * 4 + 1] = 255;
+        raw_image[cy * 4 * width + cx * 4 + 2] = 255;
+
+        pos = pos + vel;
+        vel = vel + gravity + wind;
     }
 
     raw_image
