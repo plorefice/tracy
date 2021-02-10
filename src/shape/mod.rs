610d24be92abcfde17ba93708b69d6a0fc4b1337
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-02-10 21:58:12 +0100

    shape: split each shape in its module

--- src/shape/mod.rs
+++ src/shape/mod.rs
@@ -1,11 +1,12 @@
 //! Collision shapes supported by the ray tracer.
 
+mod sphere;
+
+pub use sphere::*;
+
 use std::{fmt::Debug, ops::Deref, sync::Arc};
 
-use crate::{
-    math::Coords,
-    query::{Ray, RayCast, RayIntersection, RayIntersections},
-};
+use crate::query::RayCast;
 
 /// Trait implemented by all supported shapes.
 pub trait Shape: 'static + Debug + Send + Sync {
@@ -40,40 +41,5 @@ impl Deref for ShapeHandle {
 
     fn deref(&self) -> &Self::Target {
         &*self.0
-    }
-}
-
-/// A sphere.
-#[derive(Debug)]
-pub struct Sphere;
-
-impl Shape for Sphere {
-    #[inline]
-    fn as_ray_cast(&self) -> Option<&dyn RayCast> {
-        Some(self)
-    }
-}
-
-impl RayCast for Sphere {
-    fn intersects_ray(&self, ray: &Ray) -> Option<RayIntersections> {
-        let distance = ray.origin - Coords::from_point(0., 0., 0.);
-
-        let a = ray.dir.dot(&ray.dir);
-        let b = 2. * ray.dir.dot(&distance);
-        let c = distance.dot(&distance) - 1.;
-
-        let discriminant = b * b - 4. * a * c;
-
-        if discriminant < 0. {
-            None
-        } else {
-            Some(RayIntersections {
-                intersections: vec![
-                    RayIntersection::new((-b - discriminant.sqrt()) / (2. * a)),
-                    RayIntersection::new((-b + discriminant.sqrt()) / (2. * a)),
-                ]
-                .into_iter(),
-            })
-        }
     }
 }
---
+++ src/shape/sphere.rs
@@ -0,0 +1,43 @@
+//! The unit sphere shape.
+
+use crate::{
+    math::Coords,
+    query::{Ray, RayCast, RayIntersection, RayIntersections},
+};
+
+use super::Shape;
+
+/// The unit sphere.
+#[derive(Debug)]
+pub struct Sphere;
+
+impl Shape for Sphere {
+    #[inline]
+    fn as_ray_cast(&self) -> Option<&dyn RayCast> {
+        Some(self)
+    }
+}
+
+impl RayCast for Sphere {
+    fn intersects_ray(&self, ray: &Ray) -> Option<RayIntersections> {
+        let distance = ray.origin - Coords::from_point(0., 0., 0.);
+
+        let a = ray.dir.dot(&ray.dir);
+        let b = 2. * ray.dir.dot(&distance);
+        let c = distance.dot(&distance) - 1.;
+
+        let discriminant = b * b - 4. * a * c;
+
+        if discriminant < 0. {
+            None
+        } else {
+            Some(RayIntersections {
+                intersections: vec![
+                    RayIntersection::new((-b - discriminant.sqrt()) / (2. * a)),
+                    RayIntersection::new((-b + discriminant.sqrt()) / (2. * a)),
+                ]
+                .into_iter(),
+            })
+        }
+    }
+}
