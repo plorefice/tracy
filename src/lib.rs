845f3e153001e39bff2cab6d8157afd9f5bd2d72
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-02-11 19:05:17 +0100

    scenes: rename to scene

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,10 @@
 pub mod canvas;
 pub mod math;
 pub mod query;
-pub mod scenes;
+pub mod scene;
 pub mod shape;
 
-use scenes::SceneSize;
+use scene::SceneSize;
 use wasm_bindgen::{prelude::*, Clamped};
 use web_sys::{CanvasRenderingContext2d, ImageData};
 
@@ -28,7 +28,7 @@ pub fn init() {
 /// `UNDEFINED` is returned if `id` is not a valid scene identifier.
 #[wasm_bindgen(js_name = getCanvasSize)]
 pub fn get_canvas_size(id: String) -> Result<SceneSize, JsValue> {
-    Ok(scenes::get_config(id).ok_or(JsValue::UNDEFINED)?.size)
+    Ok(scene::get_config(id).ok_or(JsValue::UNDEFINED)?.size)
 }
 
 /// Renders scene `id` to `ctx`.
@@ -36,7 +36,7 @@ pub fn get_canvas_size(id: String) -> Result<SceneSize, JsValue> {
 /// `UNDEFINED` is returned if `id` is not a valid scene identifier.
 #[wasm_bindgen]
 pub fn draw(ctx: &CanvasRenderingContext2d, id: String) -> Result<(), JsValue> {
-    let config = scenes::get_config(id).ok_or(JsValue::UNDEFINED)?;
+    let config = scene::get_config(id).ok_or(JsValue::UNDEFINED)?;
 
     let scene = (config.render_fn)(config.size.width, config.size.height);
     let pixels = scene
