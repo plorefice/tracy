e47ce8a4a5d62fe39141bad519f3b99ef606c84c
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-02-12 20:20:07 +0100

    src: simplify code layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
 
-ReactDOM.render(
+import { Projectile } from './scenes/projectile';
+import { Clock } from './scenes/clock';
+import { FlatSphere } from './scenes/flat-sphere';
+import { PhongSphere } from './scenes/phong';
+
+// In chapter order
+const SCENES = [
+  new Projectile(),
+  new Clock(),
+  new FlatSphere(),
+  new PhongSphere(),
+]
+
+class SceneSelector extends React.Component {
+  render() {
+    const sceneList = [];
+    for (const scene of SCENES) {
+      sceneList.push(
+        <a href="#" className="list-group-item list-group-item-action">{scene.title}</a>
+      );
+    }
+
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="list-group col-md-6" >
+            {sceneList}
+          </div >
+        </div >
+      </div >
+    );
+  }
+}
+
+render(
   <React.StrictMode>
-    <App />
+    <SceneSelector />
   </React.StrictMode>,
   document.getElementById('root')
 );
