d922b7199805d601740de362e7eb03a3b2900242
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-02-13 22:49:57 +0100

    Fix GH pages hosting v3

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { HashRouter as Router, Link, Route } from 'react-router-dom';
 
 import "./index.scss";
 
