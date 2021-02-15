7958764210c95a57c74392dce6ef67888e512934
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-02-15 18:00:27 +0100

    tracy/tests: check that world-ray intersections are sorted

--- tracy/tests/ch07.rs
+++ tracy/tests/ch07.rs
@@ -3,17 +3,21 @@ use std::convert::Infallible;
 use cucumber_rust::{async_trait, gherkin::Step, given, then, when, WorldInit};
 use tracy::{
     canvas::Color,
-    math::{MatrixN, Point},
-    query::{Object, World},
+    math::{MatrixN, Point, Vector},
+    query::{Object, Ray, RayIntersection, World},
     rendering::{Material, PointLight},
     shape::{ShapeHandle, Sphere},
 };
+
+const EPSILON: f32 = 1e-4;
 
 #[derive(WorldInit)]
 pub struct TestRunner {
     world: World,
     light: PointLight,
+    ray: Ray,
     ss: Vec<Object>,
+    xs: Vec<(Object, RayIntersection)>,
 }
 
 #[async_trait(?Send)]
@@ -24,7 +28,9 @@ impl cucumber_rust::World for TestRunner {
         Ok(Self {
             world: World::new(),
             light: PointLight::default(),
+            ray: Ray::new(Point::default(), Vector::default()),
             ss: Vec::new(),
+            xs: Vec::new(),
         })
     }
 }
@@ -32,6 +38,11 @@ impl cucumber_rust::World for TestRunner {
 #[given("w ← world()")]
 async fn given_a_world(tr: &mut TestRunner) {
     tr.world = World::new();
+}
+
+#[given("w ← default_world()")]
+async fn given_a_default_world(tr: &mut TestRunner) {
+    tr.world = World::default();
 }
 
 #[allow(clippy::many_single_char_names)]
@@ -42,6 +53,14 @@ async fn given_a_point_light(tr: &mut TestRunner, x: f32, y: f32, z: f32, r: f32
         color: Color::new(r, g, b),
         intensity: 1.,
     };
+}
+
+#[given(regex = r"r ← ray\(point\((.*), (.*), (.*)\), vector\((.*), (.*), (.*)\)\)")]
+async fn given_a_ray(tr: &mut TestRunner, px: f32, py: f32, pz: f32, vx: f32, vy: f32, vz: f32) {
+    tr.ray = Ray::new(
+        Point::from_point(px, py, pz),
+        Vector::from_vector(vx, vy, vz),
+    );
 }
 
 #[given(regex = r"s([12]) ← sphere\(\) with:")]
@@ -104,6 +123,15 @@ async fn world_is_default(tr: &mut TestRunner) {
     tr.world = World::default();
 }
 
+#[when("xs ← intersect_world(w, r)")]
+async fn ray_intersects_world(tr: &mut TestRunner) {
+    tr.xs = tr
+        .world
+        .interferences_with_ray(&tr.ray)
+        .map(|(obj, x)| (obj.clone(), x))
+        .collect();
+}
+
 #[then("w contains no objects")]
 async fn world_contains_no_objects(tr: &mut TestRunner) {
     assert_eq!(tr.world.objects().count(), 0);
@@ -119,7 +147,7 @@ async fn world_has_light(tr: &mut TestRunner) {
     assert_eq!(*tr.world.lights().next().unwrap(), tr.light);
 }
 
-#[then(regex = r"w contains s([12])")]
+#[then(regex = r"^w contains s([12])$")]
 async fn world_contains_sphere(tr: &mut TestRunner, idx: String) {
     let idx = idx.parse::<usize>().unwrap() - 1;
 
@@ -128,6 +156,16 @@ async fn world_contains_sphere(tr: &mut TestRunner, idx: String) {
 
     assert_eq!(res.transform(), exp.transform());
     assert_eq!(res.material(), exp.material());
+}
+
+#[then(regex = r"^xs.count = (.*)$")]
+async fn intersection_count(tr: &mut TestRunner, n: usize) {
+    assert_eq!(tr.xs.len(), n);
+}
+
+#[then(regex = r"^xs\[(.*)\]\.t = (.*)$")]
+async fn intersection_toi(tr: &mut TestRunner, i: usize, toi: f32) {
+    assert!((tr.xs[i].1.toi - toi).abs() < EPSILON);
 }
 
 #[tokio::main]
