d95e48b31db5d66e5df82557b1072cdbfe0c4994
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-02-21 00:28:04 +0100

    ui/scene: render chapter 10 from external scene file

--- tracy-ui/src/scene/ch10.rs
+++ tracy-ui/src/scene/ch10.rs
@@ -1,12 +1,10 @@
-use std::f32::consts::PI;
+use std::fs::File;
 
 use anyhow::Result;
 use imgui::*;
 use tracy::{
-    math::{Matrix, Point3, Vec3},
-    query::{Object, World},
-    rendering::{Camera, Canvas, Color, Material, Pattern, PatternKind, PointLight},
-    shape::{Plane, Sphere},
+    query::World,
+    rendering::{Canvas, ScenePrefab},
 };
 
 use super::Scene;
@@ -24,196 +22,11 @@ impl Default for Patterns {
 }
 
 impl Patterns {
-    const SUBSCENES: [&'static str; 3] = ["Basic patterns", "Nested patterns", "Blended patterns"];
-
-    fn setup_scene(&self, width: u32, height: u32) -> (World, Camera) {
-        let mut world = World::new();
-
-        world.set_light(PointLight {
-            position: Point3::new(-10.0, 10.0, -10.0),
-            ..Default::default()
-        });
-
-        let camera = Camera::new_with_transform(
-            width,
-            height,
-            PI / 3.0,
-            Matrix::look_at(
-                Point3::new(0.0, 1.5, -4.0),
-                Point3::new(0.0, 0.5, 0.0),
-                Vec3::unit_y(),
-            ),
-        );
-
-        (world, camera)
-    }
-
-    fn render_basic(&self, width: u32, height: u32) -> Canvas {
-        let (mut world, camera) = self.setup_scene(width, height);
-
-        // Floor
-        world.add(Object::new_with_material(
-            Plane,
-            Matrix::identity(4),
-            Material {
-                pattern: Pattern::new_with_transform(
-                    PatternKind::Checkers(
-                        Box::new(Pattern::new(Color::new(0.5, 0.5, 0.5).into())),
-                        Box::new(Pattern::new(Color::new(0.2, 0.2, 0.2).into())),
-                    ),
-                    Matrix::from_translation(0.0, 0.01, 0.0),
-                ),
-                specular: 0.0,
-                ..Default::default()
-            },
-        ));
-
-        // Wall
-        world.add(Object::new_with_material(
-            Plane,
-            Matrix::from_translation(0.0, 0.0, 2.0) * Matrix::from_rotation_x(PI / 2.0),
-            Material {
-                pattern: Pattern::new_with_transform(
-                    PatternKind::Stripes(
-                        Box::new(Pattern::new(Color::new(0.5, 0.5, 0.5).into())),
-                        Box::new(Pattern::new(Color::new(0.2, 0.2, 0.2).into())),
-                    ),
-                    Matrix::from_rotation_y(PI / 4.0),
-                ),
-                specular: 0.0,
-                ..Default::default()
-            },
-        ));
-
-        // Left sphere
-        world.add(Object::new_with_material(
-            Sphere,
-            Matrix::from_translation(-1.0, 1.0, 0.0),
-            Material {
-                pattern: Pattern::new_with_transform(
-                    PatternKind::Rings(
-                        Box::new(Pattern::new(Color::new(0.0, 0.8, 0.0).into())),
-                        Box::new(Pattern::new(Color::new(0.0, 0.5, 0.0).into())),
-                    ),
-                    Matrix::from_rotation_x(-PI / 4.0)
-                        * Matrix::from_rotation_y(PI / 3.0)
-                        * Matrix::from_scale(0.22, 0.22, 0.22),
-                ),
-                specular: 0.0,
-                ..Default::default()
-            },
-        ));
-
-        // Right sphere
-        world.add(Object::new_with_material(
-            Sphere,
-            Matrix::from_translation(1.0, 0.5, -1.0) * Matrix::from_scale(0.5, 0.5, 0.5),
-            Material {
-                pattern: Pattern::new_with_transform(
-                    PatternKind::LinearGradient(
-                        Color::new(0.8, 0.0, 0.0),
-                        Color::new(0.0, 0.8, 0.0),
-                    ),
-                    Matrix::from_translation(1.0, 0.0, 0.0) * Matrix::from_scale(2.0, 2.0, 2.0),
-                ),
-                specular: 0.0,
-                ..Default::default()
-            },
-        ));
-
-        // Middle sphere
-        world.add(Object::new_with_material(
-            Sphere,
-            Matrix::from_translation(0.0, 0.4, -2.0) * Matrix::from_scale(0.4, 0.4, 0.4),
-            Material {
-                pattern: Pattern::new_with_transform(
-                    PatternKind::RadialGradient(
-                        Color::new(0.0, 0.8, 1.0),
-                        Color::new(0.0, 0.5, 0.7),
-                    ),
-                    Matrix::from_rotation_y(PI / 4.0)
-                        * Matrix::from_rotation_x(-PI / 2.0)
-                        * Matrix::from_scale(0.21, 0.21, 0.21),
-                ),
-                specular: 0.0,
-                ..Default::default()
-            },
-        ));
-
-        camera.render(&world)
-    }
-
-    fn render_nested(&self, width: u32, height: u32) -> Canvas {
-        let (mut world, camera) = self.setup_scene(width, height);
-
-        let p1 = Pattern::new_with_transform(
-            PatternKind::Stripes(
-                Box::new(Pattern::new(Color::new(0.5, 0.5, 0.5).into())),
-                Box::new(Pattern::new(Color::new(0.2, 0.2, 0.2).into())),
-            ),
-            Matrix::from_rotation_y(PI / 4.) * Matrix::from_scale(0.25, 0.25, 0.25),
-        );
-
-        let p2 = Pattern::new_with_transform(
-            PatternKind::Stripes(
-                Box::new(Pattern::new(Color::new(0.0, 0.3, 0.0).into())),
-                Box::new(Pattern::new(Color::new(0.0, 0.0, 0.3).into())),
-            ),
-            Matrix::from_rotation_y(-PI / 4.) * Matrix::from_scale(0.25, 0.25, 0.25),
-        );
-
-        // Floor
-        world.add(Object::new_with_material(
-            Plane,
-            Matrix::identity(4),
-            Material {
-                pattern: Pattern::new_with_transform(
-                    PatternKind::Checkers(Box::new(p1), Box::new(p2)),
-                    Matrix::from_translation(0.0, 0.01, 0.0),
-                ),
-                specular: 0.0,
-                ..Default::default()
-            },
-        ));
-
-        camera.render(&world)
-    }
-
-    fn render_blended(&self, width: u32, height: u32) -> Canvas {
-        let (mut world, camera) = self.setup_scene(width, height);
-
-        let p1 = Pattern::new_with_transform(
-            PatternKind::Stripes(
-                Box::new(Pattern::new(Color::WHITE.into())),
-                Box::new(Pattern::new(Color::new(0.0, 0.7, 0.0).into())),
-            ),
-            Matrix::from_rotation_y(PI / 4.) * Matrix::from_scale(0.5, 0.5, 0.5),
-        );
-
-        let p2 = Pattern::new_with_transform(
-            PatternKind::Stripes(
-                Box::new(Pattern::new(Color::WHITE.into())),
-                Box::new(Pattern::new(Color::new(0.0, 0.7, 0.0).into())),
-            ),
-            Matrix::from_rotation_y(-PI / 4.) * Matrix::from_scale(0.5, 0.5, 0.5),
-        );
-
-        // Floor
-        world.add(Object::new_with_material(
-            Plane,
-            Matrix::identity(4),
-            Material {
-                pattern: Pattern::new_with_transform(
-                    PatternKind::Blended(Box::new(p1), Box::new(p2)),
-                    Matrix::from_translation(0.0, 0.01, 0.0),
-                ),
-                specular: 0.0,
-                ..Default::default()
-            },
-        ));
-
-        camera.render(&world)
-    }
+    const SUBSCENES: [(&'static str, &'static str); 3] = [
+        ("Basic patterns", "scenes/ch10a.yml"),
+        ("Nested patterns", "scenes/ch10b.yml"),
+        ("Blended patterns", "scenes/ch10c.yml"),
+    ];
 }
 
 impl Scene for Patterns {
@@ -226,22 +39,31 @@ impl Scene for Patterns {
     }
 
     fn render(&self, width: u32, height: u32) -> Result<Canvas> {
-        Ok(match self.selection {
-            0 => self.render_basic(width, height),
-            1 => self.render_nested(width, height),
-            2 => self.render_blended(width, height),
-            _ => unreachable!(),
-        })
+        let scene: ScenePrefab =
+            serde_yaml::from_reader(File::open(Self::SUBSCENES[self.selection].1)?)?;
+
+        let mut world = World::new();
+
+        for obj in scene.objects.into_iter() {
+            world.add(obj);
+        }
+
+        world.set_light(scene.light);
+
+        let mut camera = scene.camera.build();
+        camera.set_size(width, height);
+
+        Ok(camera.render(&world))
     }
 
     fn draw(&mut self, ui: &Ui) -> bool {
         let mut redraw = false;
 
         if let Some(token) = ComboBox::new(&im_str!("Scene selector##{}", self.name()))
-            .preview_value(&ImString::new(Self::SUBSCENES[self.selection]))
+            .preview_value(&ImString::new(Self::SUBSCENES[self.selection].0))
             .begin(ui)
         {
-            for (i, &name) in Self::SUBSCENES.iter().enumerate() {
+            for (i, &(name, _)) in Self::SUBSCENES.iter().enumerate() {
                 if Selectable::new(&ImString::new(name)).build(ui) {
                     self.selection = i;
                     redraw = true;
